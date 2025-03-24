64afcf07d0612d5b1ed410b511c04e1471029d37
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-03-24 22:33:58 +0530

    connect

---
+++ src/components/buttons/Connect.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Connect = () => {
+  return (
+    <a className="px-3 py-1 rounded-lg bg-white text-black cursor-pointer select-none">
+      Connect
+    </a>
+  );
+};
+
+export default Connect;
