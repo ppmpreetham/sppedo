fb8234dab3b0bde36f655a15a7f884c5ab1705df
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-03-28 10:01:59 +0530

    fix: center the header

--- src/components/header.js
+++ src/components/header.js
@@ -3,9 +3,11 @@ import Connect from "./buttons/Connect";
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-between px-4 py-2">
-      <h1 className="text-5xl font-murmure">Sppedo</h1>
-      <nav className="flex-1">
+    <header className="flex items-center px-4 py-2">
+      <div className="w-1/4">
+        <h1 className="text-5xl font-murmure">Sppedo</h1>
+      </div>
+      <nav className="w-2/4">
         <ul className="flex justify-center space-x-4">
           <li>
             <a href="/">Home</a>
@@ -24,7 +26,7 @@ export default function Header() {
           </li>
         </ul>
       </nav>
-      <div className="flex space-x-4 items-center">
+      <div className="flex space-x-4 items-center justify-end w-1/4">
         <a href="/search">
           <Search className="w-6 h-6" />
         </a>
